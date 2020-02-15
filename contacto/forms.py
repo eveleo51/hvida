3d523f5cf3b999223ed36bf74730e0bf33c47945
Maria José Buitrago <majobuitrago887@gmail.com>
2020-02-15 16:40:28 -0500

    Intento arreglar con indicaciones del profe

--- contacto/forms.py
+++ contacto/forms.py
@@ -5,3 +5,10 @@ class DataForm(forms.ModelForm):
     class Meta:
         model=Contacto
         fields='__all__'
+
+
+#Formulario de hoja de vida
+#class ResumeCreate(forms.ModelForm):
+#    model = Contacto
+#    success_url = reverse_lazy('')
+#    fields='__all__'
