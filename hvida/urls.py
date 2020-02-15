f63f62a45cfc8c777747f9efdb45665d07931ee1
Maria José Buitrago <majobuitrago887@gmail.com>
2020-02-15 12:15:18 -0500

    Realizo apuntes del profe

--- hvida/urls.py
+++ hvida/urls.py
@@ -2,13 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from contacto import views
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('', views.home, name=""),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('/<int:pk>/', views.BadgetUpdate.as_view(), name='editar'),
+    path('<int:pk>/', views.BadgetUpdate.as_view(), name='editar'),
     path('/crear/', views.BadgetCreate.as_view(), name='crear'),
     path('/eliminar/<int:pk>/', views.BadgetDelete.as_view(), name='eliminar'),
     path('users/', include(('users.urls','users'), namespace='users')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
