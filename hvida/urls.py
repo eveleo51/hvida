ac670325ec80bc1033df7dd73a3a74ca1d35f55c
Evelio Antonio Hernandez <eveleo51@hotmail.com>
2020-03-20 00:57:49 -0500

    con proteccion por Url -1

--- hvida/urls.py
+++ hvida/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.decorators import login_required
 from contacto import views
-# from . import views
 
 from django.conf.urls.static import static
 from django.conf import settings
-from django.contrib.auth.decorators import login_required
+
 
 
 urlpatterns = [
--- users/urls.py
+++ users/urls.py
@@ -3,15 +3,13 @@ from.views import signup, ActivateUser, templateEmailSent, UserList
 from contacto.views import UserAppCreate, UserAppView
 from django.contrib.auth.decorators import login_required
 from . import views
+
 #, UserApp
 urlpatterns = [
     path('signup/', signup, name='signup'),
     path('activate/<str:uidb64>/<str:token>/', ActivateUser, name='activate'),
     path('emailsent/<str:username>/', templateEmailSent, name='emailsent'),
-    path('', login_required(views.UserList.as_view()), name='index'),  #Protegemos las urls de Userapp Probando 123....
-#    path('', login_required(views.base.as_view()), name='index'),  #Protegemos las urls de Userapp Probando 123....
-#    path('', login_required('auth:users'), name='index'),  #Protegemos las urls de Userapp Probando 123....
-    path('listar/', UserList.as_view(), name="listar"),
-    path('listcontact/', UserAppView.as_view(), name="listcontact"),
-    path('userapp/', UserAppCreate.as_view(), name="userapp")
+    path('listar/', login_required(views.UserList.as_view()), name='listar'),  #Protegemos la url de listado de usuarios
+    path('listcontact/', login_required(UserAppView.as_view()), name='listcontact'),  #Protegemos las url de listado de contactos
+    path('userapp/', login_required(UserAppCreate.as_view()), name='userapp'),  #Protegemos las url de agregar hoja de vida
 ]
